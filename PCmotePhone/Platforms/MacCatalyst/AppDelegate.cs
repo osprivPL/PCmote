266a9031c22bfab03e86f6d832b6e87bca3d2280
snapm <michalOzdz@icloud.com>
2026-03-12 21:27:52 +0100

    test

---
+++ PCmotePhone/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PCmotePhone
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
